49050ffcb4c93748117a5456e66d9836b9db340b
Jordyn Thompson <jordyn.poppy04@gmail.com>
2020-03-21 15:13:47 +1300

    New idea

--- Login_Screen/js/app.js
+++ Login_Screen/js/app.js
@@ -5,3 +5,20 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http, $filter){
   $scope.test = "Test Successful!";
   
 }]);
+
+// //loginController-js 
+// var app = angular.module('myApp');
+// app.controller('LoginController', function($scope, $rootScope, $stateParams, $state, LoginService) {
+//    $scope.formSubmit = function() {
+//      if(LoginService.login($scope.username, $scope.password)) {
+//        $rootScope.userName = $scope.username;
+//        $scope.error = '';
+//        $scope.username = '';
+//        $scope.password = '';
+//        $state.transitionTo('home');
+//      } else {
+//        $scope.error = "Incorrect username/password !";
+//      }   
+//    };    
+//  });
+
