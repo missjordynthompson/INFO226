c816b5b8a3dc847e0534776d753316f5dad117e0
Jordyn Thompson <jordyn.poppy04@gmail.com>
2020-05-17 22:17:46 +1200

    Make nav bar scroll with page scroll

--- WBSG/app.js
+++ WBSG/app.js
@@ -1,7 +1,6 @@
 var app = angular.module('plunker', []);
 
 app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http, $filter) {
-
 	$scope.showLogin = true;
 	$scope.showDashboard = false;
 	$scope.studentUser = false;
@@ -15,11 +14,23 @@ app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http, $filter)
 		["jake", "1234"],
 		["jordyn", "1234"],
 		["jesse", "1234"]
-	]);
+    ]);
+    
+	$scope.getcourses = "https://caab.sim.vuw.ac.nz/api/tukukipaul/course_directory.json";
+  	$http.get($scope.getcourses)
+    .then(function successCall(response) {
+      $scope.coursedata = response.data.courses;
+	});
+	
+	$scope.getassignments = "https://caab.sim.vuw.ac.nz/api/tukukipaul/assignment_directory.json";
+  	$http.get($scope.getassignments)
+    .then(function successCall(response) {
+      $scope.assignmentdata = response.data.assignments;
+    });
 	
 	//Simple credential check function
 	$scope.checkCred = function () {
-		alert("HI");
+		// alert("HI");
 		if ($scope.userInp != null && $scope.userPwd != null) {
 			if (sampleCredentials.has($scope.userInp)) {
 				if ($scope.userPwd == sampleCredentials.get($scope.userInp)) {
@@ -48,5 +59,18 @@ app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http, $filter)
 				content.style.maxHeight = content.scrollHeight + "px"; // if minimized, click to expand
 			}
 		}
+	}
+	
+	/*----- For Nav Bar -----*/
+	window.onscroll = function() { myFunction() };
+
+	var header = document.getElementById("myHeader");
+	var sticky = header.offsetHeight;
+	function myFunction() {
+	if (window.pageYOffset >= sticky) {
+		header.classList.add("sticky");
+	} else {
+		header.classList.remove("sticky");
+	}
 	}
 }]);
